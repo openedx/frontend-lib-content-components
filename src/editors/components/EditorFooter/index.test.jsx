babd6d78f3b94740d4cd0a18ea72d99dbac10516
connorhaugh <chaugh21@amherst.edu>
2022-02-22 11:34:22 -0500

    test: add EditorFooter tests

--- src/editors/components/EditorFooter/index.test.jsx
+++ src/editors/components/EditorFooter/index.test.jsx
@@ -1,104 +1,96 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { mount } from 'enzyme';
-import EditorFooter from './EditorFooter';
-import EditorPageContext from './EditorPageContext';
-import { ActionStates } from './data/constants';
-import EditorPageProvider from './EditorPageProvider';
-import { saveBlock } from './data/api';
+import { shallow } from 'enzyme';
+import * as module from './index';
+import { selectors, thunkActions } from '../../data/redux';
+import { RequestKeys } from '../../data/constants/requests';
+import { saveTextBlock, navigateCallback } from '../../hooks';
 
-const locationTemp = window.location;
-beforeAll(() => {
-  delete window.location;
-  window.location = {
-    assign: jest.fn(),
-  };
-});
-afterAll(() => {
-  window.location = locationTemp;
-});
-
-jest.mock('./data/api', () => {
-  const originalModule = jest.requireActual('./data/api');
-  // Mock the default export and named export saveBlock
-  return {
-    __esModule: true,
-    ...originalModule,
-    saveBlock: jest.fn(() => {}),
-  };
-});
-
-jest.spyOn(React, 'useRef').mockReturnValue({
-  current: {
-    getContent: () => '',
-  },
-});
-
-test('Rendering: loaded', () => {
-  const context = {
-    unitUrlLoading: ActionStates.FINISHED,
-  };
-  render(
-    <EditorPageContext.Provider value={context}>
-      <EditorFooter />
-    </EditorPageContext.Provider>,
-  );
-  expect(screen.getByText('Cancel')).toBeTruthy();
-  expect(screen.getByText('Add To Course')).toBeTruthy();
-});
-
-test('Rendering: loading url', () => {
-  const context = {
-    unitUrlLoading: ActionStates.NOT_BEGUN,
-  };
-  render(
-    <EditorPageContext.Provider value={context}>
-      <EditorFooter />
-    </EditorPageContext.Provider>,
-  );
-  expect(screen.getByText('Cancel')).toBeTruthy();
-  expect(screen.getAllByRole('button', { 'aria-label': 'Save' })).toBeTruthy();
-  expect(screen.queryByText('Add To Course')).toBeNull();
-});
-
-test('Navigation: Cancel', () => {
-  const context = {
-    unitUrlLoading: ActionStates.FINISHED,
-    unitUrl: {
-      data: {
-        ancestors:
-        [
-          { id: 'fakeblockid' },
-        ],
-      },
+jest.mock('../../data/redux', () => ({
+  thunkActions: {
+    app: {
+      saveBlock: jest.fn().mockName('thunkActions.app.saveBlock'),
     },
-    studioEndpointUrl: 'Testurl',
+  },
+  selectors: {
+    app: {
+      isInitialized: jest.fn(state => ({ isInitialized: state })),
+      studioEndpointUrl: jest.fn(state => ({ studioEndpointUrl: state })),
+    },
+    requests: {
+      // eslint-disable-next-line no-unused-vars
+      isFailed: jest.fn((state, requestkey) => ({ isFailed: state })),
+    },
+  },
+}));
+
+jest.mock('.', () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual('./index');
+  return {
+    __esModule: true, // Use it when dealing with esModules
+    ...originalModule,
+    handleCancelClicked: jest.fn().mockName('handleCancelClicked'),
+    handleSaveClicked: jest.fn().mockName('handleSaveClicked'),
   };
-  render(
-    <EditorPageContext.Provider value={context}>
-      <EditorFooter />
-    </EditorPageContext.Provider>,
-  );
-  expect(screen.getByText('Cancel')).toBeTruthy();
-  userEvent.click(screen.getByText('Cancel'));
-  expect(window.location.assign).toHaveBeenCalled();
 });
 
-test('Navigation: Save', () => {
-  const wrapper = mount(
-    <EditorPageProvider
-      blockType="html"
-      courseId="myCourse101"
-      blockId="redosablocksalot"
-      studioEndpointUrl="celaicboss.axe"
-    >
-      <EditorFooter />
-    </EditorPageProvider>,
-  );
-  const button = wrapper.find({ children: 'Add To Course' });
-  expect(button).toBeTruthy();
-  button.simulate('click');
-  expect(saveBlock).toHaveBeenCalled();
-  expect(window.location.assign).toHaveBeenCalled();
+jest.mock('../../hooks', () => ({
+  saveTextBlock: jest.fn(),
+  navigateCallback: jest.fn(),
+}));
+
+describe('EditorFooter', () => {
+  describe('Component', () => {
+    const props = {
+      editorRef: jest.fn(),
+      isInitialized: true,
+      returnUrl: 'hocuspocus.ca',
+      saveFailed: false,
+      saveBlock: jest.fn(),
+    };
+    test('renders as expected with default behavior', () => {
+      expect(shallow(<module.EditorFooter {...props} />)).toMatchSnapshot();
+    });
+    test('not intialized, Spinner appears and button is disabled', () => {
+      expect(shallow(<module.EditorFooter {...props} isInitialized={false} />)).toMatchSnapshot();
+    });
+    test('Save Failed, error message raised', () => {
+      expect(shallow(<module.EditorFooter {...props} saveFailed />)).toMatchSnapshot();
+    });
+    describe('On Click Handlers', () => {
+      const realmodule = jest.requireActual('./index');
+      test('handleSaveClicked calls saveTextBlock', () => {
+        const createdCallback = realmodule.handleSaveClicked(props);
+        createdCallback();
+        expect(saveTextBlock).toHaveBeenCalled();
+      });
+      test('handleCancelClicked calls navigateCallback', () => {
+        realmodule.handleCancelClicked({ returnUrl: props.returnUrl });
+        expect(navigateCallback).toHaveBeenCalledWith(props.returnUrl);
+      });
+    });
+  });
+  describe('MapStateToProps', () => {
+    const testState = { A: 'pple', B: 'anana', C: 'ucumber' };
+    test('isInitialized from app.isInitialized', () => {
+      expect(
+        module.mapStateToProps(testState).isInitialized,
+      ).toEqual(selectors.app.isInitialized(testState));
+    });
+    test('studioEndpointUrl from app.studioEndpointUrl', () => {
+      expect(
+        module.mapStateToProps(testState).studioEndpointUrl,
+      ).toEqual(selectors.app.studioEndpointUrl(testState));
+    });
+    test('saveFailed from requests.isFailed', () => {
+      expect(
+        module.mapStateToProps(testState).saveFailed,
+      ).toEqual(selectors.requests.isFailed(testState, { requestKey: RequestKeys.saveBlock }));
+    });
+  });
+  describe('MapDispatchToProps', () => {
+    test('saveBlock from thunkActions.app.saveBlock', () => {
+      expect(module.mapDispatchToProps.saveBlock).toEqual(thunkActions.app.saveBlock);
+    });
+  });
 });
