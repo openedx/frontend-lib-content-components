3dff2a912a2b85b84162e7cf8cd84dda42dd48c6
Ben Warzeski <bwarzeski@edx.org>
2022-04-21 10:05:34 -0400

    Update src/editors/containers/VideoEditor/components/VideoSettingsModal/components/CollapsibleFormWidget.jsx
    Co-authored-by: connorhaugh <49422820+connorhaugh@users.noreply.github.com>

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/CollapsibleFormWidget.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/CollapsibleFormWidget.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Collapsible } from '@edx/paragon';
 
 /**
- * Simple Wrapper for Form Widget components in the Video Settings modal
+ * Simple Wrapper for a Form Widget component in the Video Settings modal
  * Takes a title element and children, and produces a collapsible widget container
  * <CollapsibleFormWidget title={<h1>My Title</h1>}>
  *   <div>My Widget</div>
