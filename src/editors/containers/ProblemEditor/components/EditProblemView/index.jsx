7e136f0498e9699146b4bbe2428939f516180495
connorhaugh <chaugh21@amherst.edu>
2022-05-12 17:04:32 -0400

    feat: skeleton P1

---
+++ src/editors/containers/ProblemEditor/components/EditProblemView/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { AnswerWidget } from './AnswerWidget';
+import { SettingsWidget } from './SettingsWidget';
+import { QuestionWidget } from './QuestionWidget';
+import { EditorContainer } from '../../../../containers/EditorContainer'
+
+export default function EditProblemView() {
+  return (
+    <div>
+      <EditorContainer>
+        <div>
+          <h1>
+            Edit Problem View
+          </h1>
+        </div>
+        <AnswerWidget />
+        <SettingsWidget />
+        <QuestionWidget />
+      </EditorContainer>
+    </div>
+  );
+}
