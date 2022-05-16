b7b5fc6b78e2911c49019bddc4343f9c0033fce5
connorhaugh <chaugh21@amherst.edu>
2022-05-16 09:23:40 -0400

    docs: add docstrings to texteditor hooks

--- src/editors/containers/TextEditor/hooks.js
+++ src/editors/containers/TextEditor/hooks.js
@@ -16,6 +16,16 @@ export const state = StrictDict({
   refReady: (val) => useState(val),
 });
 
+/**
+ * Create A function which takes in a tinyMCE editor instance and adds custom
+ * features to the toolbars and UI.
+ *
+ * @param {openModal} func - a function which opens the custom image modal.
+ * @param {setImage} func - a function which sets the current image selected in the image modal.
+ * @return {func} - takes in a tinyMCE editor instance and adds custom
+ * features to the toolbars and UI.
+ */
+
 export const setupCustomBehavior = ({ openModal, setImage }) => (editor) => {
   // image upload button
   editor.ui.registry.addButton(tinyMCE.buttons.imageUploadButton, {
@@ -35,6 +45,17 @@ export const setupCustomBehavior = ({ openModal, setImage }) => (editor) => {
     onAction: openCodeEditor,
   });
 };
+
+/**
+ * Setup tinyMCE editor with chosen inputs
+ *
+ * @param {setEditorRef} func - a function which sets the higher-level reference to the editor.
+ * @param {blockValue} object - the xblock object which contains the html.
+ * @param {openModal} func - a function which opens the custom image modal.
+ * @param {setSelection} func - a function which sets the current image selected in the image modal.
+ * @param {initializeEditor} func - a function which tells the higher level data abstraction
+ * the editor is ready to be displayed.
+ */
 
 export const editorConfig = ({
   setEditorRef,
@@ -58,6 +79,16 @@ export const editorConfig = ({
   },
 });
 
+/**
+ * A stateful toggle for opening/closing a modal.
+ * @return {object} - An with toggle open/closed functions and a state reference.
+ *
+ * usage:
+ *   const {isModalOpen: isOpen, openModal, closeModal} = modalToggle();
+ *   openModal();
+ *   closeModal();
+ */
+
 export const modalToggle = () => {
   const [isOpen, setIsOpen] = module.state.isModalOpen(false);
   return {
@@ -66,6 +97,20 @@ export const modalToggle = () => {
     closeModal: () => setIsOpen(false),
   };
 };
+
+/**
+ * Creates a function which opens the image modal with the selected image when called.
+ *
+ * @param {editor} object - a tinyMCE editor
+ * @param {openModal} func - a function which opens the custom image modal.
+ * @param {setImage} func - a function which sets the current image selected in the image modal.
+ *
+ * @return {func} - An with toggle open/closed functions and a state reference.
+ *
+ * usage:
+ *   const openModal= openModalWithSelectedImage ({editor: currentMCE, setImage, openModal})
+ *   openModal();
+ */
 
 export const openModalWithSelectedImage = ({ editor, setImage, openModal }) => () => {
   const imgHTML = editor.selection.getNode();
@@ -87,6 +132,13 @@ export const prepareEditorRef = () => {
   useEffect(() => setRefReady(true), []);
   return { editorRef, refReady, setEditorRef };
 };
+
+/**
+ * Creates a function which returns the HTML content of the tinyMCE Editor
+ *
+ * @param {editorRef} object - a reference to the tinyMCE editor instance in the DOM
+ * @return {func} - a function which returns the content of the editor.
+ */
 
 export const getContent = ({ editorRef }) => () => editorRef.current?.getContent();
 
