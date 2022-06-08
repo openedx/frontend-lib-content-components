c6ad9f31a85306c401c5631b6401db585db7909c
connorhaugh <chaugh21@amherst.edu>
2022-06-08 09:51:32 -0400

    fix: improve coverage

--- src/editors/data/services/cms/urls.test.js
+++ src/editors/data/services/cms/urls.test.js
@@ -1,5 +1,7 @@
 import {
+  returnUrl,
   unit,
+  libraryV1,
   block,
   blockAncestor,
   courseAssets,
@@ -9,8 +11,10 @@ import {
 describe('cms url methods', () => {
   const studioEndpointUrl = 'urLgoeStOstudiO';
   const blockId = 'blOckIDTeST123';
-  const learningContextId = 'learningContextId321';
-  describe('unit', () => {
+  const learningContextId = 'lEarnIngCOntextId123';
+  const courseId = 'course-v1:courseId123';
+  const libraryV1Id = 'library-v1:libaryId123';
+  describe('return to learning context urls', () => {
     const unitUrl = {
       data: {
         ancestors: [
@@ -20,6 +24,22 @@ describe('cms url methods', () => {
         ],
       },
     };
+    it('returns the library page when given the library', () => {
+      expect(returnUrl({ studioEndpointUrl, unitUrl, learningContextId: libraryV1Id }))
+        .toEqual(`${studioEndpointUrl}/library/${libraryV1Id}`);
+    });
+    it('returns url with studioEndpointUrl and unitUrl', () => {
+      expect(returnUrl({ studioEndpointUrl, unitUrl, learningContextId: courseId }))
+        .toEqual(`${studioEndpointUrl}/container/${unitUrl.data.ancestors[0].id}`);
+    });
+    it('returns empty string if no unit url', () => {
+      expect(returnUrl({ studioEndpointUrl, unitUrl: null, learningContextId: courseId }))
+        .toEqual('');
+    });
+    it('returns the library page when given the library', () => {
+      expect(libraryV1({ studioEndpointUrl, learningContextId: libraryV1Id }))
+        .toEqual(`${studioEndpointUrl}/library/${libraryV1Id}`);
+    });
     it('returns url with studioEndpointUrl and unitUrl', () => {
       expect(unit({ studioEndpointUrl, unitUrl }))
         .toEqual(`${studioEndpointUrl}/container/${unitUrl.data.ancestors[0].id}`);
