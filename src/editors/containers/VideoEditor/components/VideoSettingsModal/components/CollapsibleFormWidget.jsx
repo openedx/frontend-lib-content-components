a434531d3e08997b89ece47fdae660e21d12a238
rayzhou-bit <rzhou@2u.com>
2022-08-24 12:08:59 -0400

    feat: wip1

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/CollapsibleFormWidget.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/CollapsibleFormWidget.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Collapsible } from '@edx/paragon';
+import { Collapsible, Icon, IconButton } from '@edx/paragon';
+import { ExpandLess, ExpandMore } from '@edx/paragon/icons';
 
 /**
  * Simple Wrapper for a Form Widget component in the Video Settings modal
@@ -11,16 +12,38 @@ import { Collapsible } from '@edx/paragon';
  * </CollapsibleFormWidget>
  */
 export const CollapsibleFormWidget = ({
-  title,
   children,
+  title,
 }) => (
-  <Collapsible defaultOpen title={title}>
-    {children}
-  </Collapsible>
+  <Collapsible.Advanced
+    className="collapsible-card rounded mb-3 px-3 py-2"
+    defaultOpen
+  >
+    <Collapsible.Trigger
+      className="collapsible-trigger d-flex border-0 align-items-center"
+      style={{ justifyContent: 'unset' }}
+    >
+      <div className="d-flex flex-grow-1 w-75">{title}</div>
+      <Collapsible.Visible whenClosed>
+        <div className="align-self-start">
+          <IconButton alt="TODO" src={ExpandMore} iconAs={Icon} variant="dark" />
+        </div>
+      </Collapsible.Visible>
+      <Collapsible.Visible whenOpen>
+        <div className="align-self-start">
+          <IconButton alt="TODO" src={ExpandLess} iconAs={Icon} variant="dark" />
+        </div>
+      </Collapsible.Visible>
+    </Collapsible.Trigger>
+    <Collapsible.Body className="collapsible-body rounded px-0">
+      {children}
+    </Collapsible.Body>
+  </Collapsible.Advanced>
 );
 CollapsibleFormWidget.propTypes = {
-  title: PropTypes.node.isRequired,
   children: PropTypes.node.isRequired,
+  collapsedDisplay: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
 };
 
 export default CollapsibleFormWidget;
