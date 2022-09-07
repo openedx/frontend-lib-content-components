dea843b10e9be150856065308f8270d458b3a578
Farhaan Bukhsh <farhaan@opencraft.com>
2022-09-07 21:20:36 +0530

    fix: Remove unwanted data and using loadash
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- src/editors/data/redux/thunkActions/problem.js
+++ src/editors/data/redux/thunkActions/problem.js
@@ -1,17 +1,14 @@
+import { isEmpty, get } from 'lodash-es';
 import { actions } from '..';
 import { parseMarkdown } from '../../../containers/ProblemEditor/data/MarkDownParser';
-import { demoProblemData } from "../../services/cms/mockVideoData";
 
 export const initializeProblem = (blockValue) => (dispatch) => {
 
-    if (blockValue === null){
-      return;
-    }
-    const problemData = blockValue.data;
-    const rawOLX = problemData.data;
-    const markdown = problemData.metadata.markdown;
-    const parsedData = parseMarkdown(markdown);
-    dispatch(actions.problem.load({...parsedData, rawOLX}));
-  };
+  const rawOLX = get(blockValue, 'data.data', {})
+  const parsedData = get(blockValue, 'data.metadata.markdown', {})
+  if (!isEmpty(parsedData) && !isEmpty(rawOLX)) {
+    dispatch(actions.problem.load({ ...parseMarkdown(parsedData), rawOLX }));
+  }
+};
 
 export default { initializeProblem };
