a9b784485d78fa2afd00114f95fb0f0d1656347a
KristinAoki <kaoki@2u.com>
2022-10-11 15:50:30 -0400

    fix: remove commented out code

--- src/editors/containers/EditorContainer/components/TitleHeader/EditConfirmationButtons.test.jsx
+++ src/editors/containers/EditorContainer/components/TitleHeader/EditConfirmationButtons.test.jsx
@@ -9,20 +9,9 @@ describe('EditConfirmationButtons', () => {
     updateTitle: jest.fn().mockName('args.updateTitle'),
     cancelEdit: jest.fn().mockName('args.cancelEdit'),
   };
-  // let el;
-  // beforeEach(() => {
-  //   el = shallow(<module.EditConfirmationButtons {...props} />);
-  // });
-
   describe('snapshot', () => {
     test('snapshot', () => {
       expect(shallow(<module.EditConfirmationButtons {...props} />)).toMatchSnapshot();
     });
-    // test('displays Edit Icon', () => {
-    //   const formControl = el.find(Form.Control);
-    //   expect(formControl.props().trailingElement).toMatchObject(
-    //     <EditConfirmationButtons updateTitle={props.updateTitle} cancelEdit={props.cancelEdit} />,
-    //   );
-    // });
   });
 });
