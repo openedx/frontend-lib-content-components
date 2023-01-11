2f33f05c6890abf7a86fe2ecd93a4061e63aa761
Jesper Hodge <jhodge@outlook.de>
2023-01-11 17:15:50 +0000

    fix: update raw editor tests

--- src/editors/sharedComponents/CodeEditor/index.jsx
+++ src/editors/sharedComponents/CodeEditor/index.jsx
@@ -20,7 +20,9 @@ export const CodeEditor = ({
 }) => {
   const DOMref = useRef();
   const btnRef = useRef();
-  hooks.createCodeMirrorDomNode({ ref: DOMref, initialText: value, upstreamRef: innerRef, lang });
+  hooks.createCodeMirrorDomNode({
+    ref: DOMref, initialText: value, upstreamRef: innerRef, lang,
+  });
   const { showBtnEscapeHTML, hideBtn } = hooks.prepareShowBtnEscapeHTML();
 
   return (
