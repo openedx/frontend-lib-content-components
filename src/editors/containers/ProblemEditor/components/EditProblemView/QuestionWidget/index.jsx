edbaa394e1944777159106ca953fb6fc1c9b47ae
Jesper Hodge <19345795+jesperhodge@users.noreply.github.com>
2023-01-19 12:46:29 -0500

    Update src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/index.jsx
    Co-authored-by: Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>

--- src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/index.jsx
+++ src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/index.jsx
@@ -19,7 +19,6 @@ export const QuestionWidget = ({
     <div className="question-widget">
       <div className="h4">
           <FormattedMessage {...messages.questionWidgetTitle} />
-        </h1>
         <Editor {
           ...hooks.problemEditorConfig({
             setEditorRef,
