ef810f783c4992ce9e43940aa1b9ef39e6f3b6d8
KristinAoki <kaoki@2u.com>
2023-03-01 14:37:39 -0500

    feat: import sett setAssetsToStatic

--- src/editors/containers/ProblemEditor/components/EditProblemView/hooks.js
+++ src/editors/containers/ProblemEditor/components/EditProblemView/hooks.js
@@ -1,5 +1,6 @@
 import ReactStateSettingsParser from '../../data/ReactStateSettingsParser';
 import ReactStateOLXParser from '../../data/ReactStateOLXParser';
+import { setAssetToStaticUrl } from '../../../../sharedComponents/TinyMceWidget/hooks';
 
 // eslint-disable-next-line import/prefer-default-export
 export const parseState = (problem, isAdvanced, ref) => () => {
