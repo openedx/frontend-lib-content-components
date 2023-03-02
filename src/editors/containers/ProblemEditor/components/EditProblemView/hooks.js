628752a8073583a63b827fdcb88fb35b9a8f3be8
KristinAoki <kaoki@2u.com>
2023-03-02 10:50:30 -0500

    feat: add setAssetToStatic to problem editor

--- src/editors/containers/ProblemEditor/components/EditProblemView/hooks.js
+++ src/editors/containers/ProblemEditor/components/EditProblemView/hooks.js
@@ -3,13 +3,19 @@ import ReactStateOLXParser from '../../data/ReactStateOLXParser';
 import { setAssetToStaticUrl } from '../../../../sharedComponents/TinyMceWidget/hooks';
 
 // eslint-disable-next-line import/prefer-default-export
-export const parseState = (problem, isAdvanced, ref) => () => {
+export const parseState = ({
+  problem,
+  isAdvanced,
+  ref,
+  assets,
+}) => () => {
   const reactSettingsParser = new ReactStateSettingsParser(problem);
   const reactOLXParser = new ReactStateOLXParser({ problem });
+  const reactBuiltOlx = setAssetToStaticUrl({ editorValue: reactOLXParser.buildOLX(), assets });
   const rawOLX = ref?.current?.state.doc.toString();
 
   return {
     settings: reactSettingsParser.getSettings(),
-    olx: isAdvanced ? rawOLX : reactOLXParser.buildOLX(),
+    olx: isAdvanced ? rawOLX : reactBuiltOlx,
   };
 };
