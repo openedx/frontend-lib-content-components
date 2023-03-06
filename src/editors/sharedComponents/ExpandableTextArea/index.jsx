db06b042e3915a01b01e342138123fb97869e1da
KristinAoki <kaoki@2u.com>
2023-03-06 16:30:40 -0500

    feat: update to use TinyMceWidget

--- src/editors/sharedComponents/ExpandableTextArea/index.jsx
+++ src/editors/sharedComponents/ExpandableTextArea/index.jsx
@@ -1,92 +1,42 @@
-import React, { useState } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { Form } from '@edx/paragon';
+import React from 'react';
 import PropTypes from 'prop-types';
+import TinyMceWidget from '../TinyMceWidget';
+import { prepareEditorRef } from '../TinyMceWidget/hooks';
+import './index.scss';
 
-export const ExpandableTextArea = ({ value, onChange, configOverrides }) => {
-  /*
-    usage:
-
-    const [value, setValue] = useState('Something');
-  return (
-    < ExpandableTextArea
-        value={value}
-        onChange = (newContent) => setValue(newContent)
-    />
-
-  );
-    */
-
-  const ref = React.useRef();
-
-  const showEditor = () => {
-    console.log('show editor');
-    console.log({ ref });
-    ref.current.show();
-  };
-
-  const [edit, setEdit] = useState(false);
-
-  const handleClick = (event) => {
-    event.stopPropagation();
-    if (!edit) {
-      setEdit(true);
-      showEditor();
-    }
-  };
-
-  const config = {
-    init: {
-      setup: (editor) => {
-        ref.current = editor;
-        editor.on('init', (e) => {
-          console.log('The Editor has initialized.');
-          editor.hide();
-        });
-        editor.on('blur', (e) => {
-          e.stopImmediatePropagation();
-          console.log('The Editor has Blurred.');
-          editor.hide();
-          setEdit(false);
-        });
-      },
-      height: 500,
-      menubar: 'file edit view',
-      plugins: [
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table paste code help wordcount',
-      ],
-    },
-    onInit: {},
-  };
+export const ExpandableTextArea = ({
+  value,
+  setContent,
+  placeholder,
+  ...props
+}) => {
+  const { editorRef, setEditorRef } = prepareEditorRef();
 
   return (
-    <>
-      {!edit && (
-        <Form.Control
-          value={value}
-          onClick={handleClick}
-        />
-      )}
-      <div style={{ display: edit ? 'block' : 'none' }}>
-        <Editor
-          init={config.init}
-          onInit={config.onInit}
-          initialValue={value}
-          value={value}
-          onEditorChange={onChange}
-        />
-      </div>
-    </>
+    <div className="expandable-mce">
+      <TinyMceWidget
+        textValue={value}
+        editorRef={editorRef}
+        editorType="expandable"
+        setEditorRef={setEditorRef}
+        updateContent={setContent}
+        placeholder={placeholder}
+        minHeight={150}
+        {...props}
+      />
+    </div>
   );
 };
 
+ExpandableTextArea.defaultProps = {
+  value: null,
+  placeholder: null,
+};
+
 ExpandableTextArea.propTypes = {
-    value: PropTypes.string,
-    onChange: PropTypes.func,
-    configOverrides: PropTypes.object,
-  };
-
-
+  value: PropTypes.string,
+  setContent: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+};
 
 export default ExpandableTextArea;
