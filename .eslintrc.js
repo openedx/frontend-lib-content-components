a41758d502dbdf70a4d3f509b13e64d1670da9a1
Jesper Hodge <jhodge@outlook.de>
2023-05-09 21:47:26 +0000

    fix: enable rules of hooks lint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ const config = createConfig('eslint', {
     'import/no-named-as-default-member': 'off',
     'import/no-self-import': 'off',
     'spaced-comment': ['error', 'always', { 'block': { 'exceptions': ['*'] } }],
-    'react-hooks/rules-of-hooks': 'off',
+    'react-hooks/rules-of-hooks': 2,
     'react-hooks/exhaustive-deps': 'off',
     'radix': 'off',
   },
