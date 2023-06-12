04c063f6c6a59413a0300fe382adb67b653ba76b
KristinAoki <kaoki@2u.com>
2023-06-12 12:07:50 -0400

    feat: add drag handle

--- src/editors/sharedComponents/DraggableList/SortableItem.jsx
+++ src/editors/sharedComponents/DraggableList/SortableItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { intlShape, injectIntl } from '@edx/frontend-platform/i18n';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { Icon, IconButtonWithTooltip, Row } from '@edx/paragon';
+import { DragIndicator } from '@edx/paragon/icons' 
+import messages from './messages';
 
-const SortableItem = ({ id, children }) => {
+const SortableItem = ({
+  id,
+  componentStyle,
+  children,
+  // injected
+  intl
+}) => {
   const {
     attributes,
     listeners,
@@ -15,18 +25,36 @@ const SortableItem = ({ id, children }) => {
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    ...componentStyle,
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+    <Row
+      ref={setNodeRef}
+      style={style}
+    >
       {children}
-    </div>
+      <IconButtonWithTooltip
+        key='drag-to-reorder-icon'
+        tooltipPlacement='top'
+        tooltipContent={intl.formatMessage(messages.tooltipContent)}
+        src={DragIndicator}
+        iconAs={Icon}
+        variant="secondary"
+        alt={intl.formatMessage(messages.tooltipContent)}
+        {...attributes}
+        {...listeners}
+      />
+    </Row>
   );
 };
 
 SortableItem.propTypes = {
   id: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  componentStyle: PropTypes.shape({}),
+  // injected
+  intl: intlShape.isRequired,
 };
 
-export default SortableItem;
+export default injectIntl(SortableItem);
