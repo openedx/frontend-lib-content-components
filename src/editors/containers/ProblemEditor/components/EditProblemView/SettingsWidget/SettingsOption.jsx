3f1e8228da13bbc28650c28d78cdbfa164308500
Taylor Preston <tpreston@2u.com>
2023-08-08 14:20:06 -0400

    feat: w-100 in SettingsOption.jsx

--- src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/SettingsOption.jsx
+++ src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/SettingsOption.jsx
@@ -18,10 +18,7 @@ export const SettingsOption = ({
 
   return (
     <Card className={`${className} settingsOption border border-light-700 shadow-none`}>
-      {
-        //HAD TO ADD W-100 TO CARD.SECTION - CHECK WITH RAY TO MAKE SURE THIS IS FINE
-      }
-      <Card.Section className="settingsCardTitleSection" key={`settingsOption-${title}-header`}>
+      <Card.Section className="settingsCardTitleSection w-100" key={`settingsOption-${title}-header`}>
         <Collapsible.Advanced
           open={isCardCollapsibleOpen}
           onToggle={toggleCardCollapse}
