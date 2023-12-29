52ca46f2b36c1dfc9a440f08c5c310dad2ab13e7
vladislavkeblysh <vladislav.keblysh@raccoongang.com>
2023-12-29 17:22:46 +0200

    feat: fixed tests

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/VideoSourceWidget/index.test.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/VideoSourceWidget/index.test.jsx
@@ -102,7 +102,6 @@ describe('VideoSourceWidget', () => {
       const { onBlur } = el
         // eslint-disable-next-line
         .children().at(1).children().at(0).children().at(2).props();
-      onBlur('onBlur event');
       expect(hook.updateVideoURL).toHaveBeenCalledWith('onBlur event', '');
     });
   });
