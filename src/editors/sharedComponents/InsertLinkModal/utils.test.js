2dcc8a67c451b29607bdb5e8cd16675dc472bfcd
Jhon Venté <jhon.vente@edunext.co>
2024-01-18 16:55:53 -0500

    test: some tests for formatblocks util

--- src/editors/sharedComponents/InsertLinkModal/utils.test.js
+++ src/editors/sharedComponents/InsertLinkModal/utils.test.js
@@ -1,5 +1,6 @@
 import {
   addPathToBlocks,
+  formatBlocks,
   getSectionsList,
   getChildrenFromList,
   filterBlocksByText,
@@ -62,6 +63,69 @@ describe('utils', () => {
 
       expect(nestedBlock2.path).toBe('Any display name / Block children 2');
       expect(nestedBlock2.parentId).toBe(rootBlockId);
+    });
+  });
+
+  describe('formatBlocks', () => {
+    const mockBlocks = {
+      blockRoot: {
+        id: 'blockRoot',
+        blockId: 'edx_block-1',
+        lmsWebUrl: 'http://localhost/weburl',
+        legacyWebUrl: 'http://localhost/legacy',
+        studentViewUrl: 'http://localhost/studentview',
+        type: 'character',
+        displayName: 'Any display name',
+        children: ['block1', 'block2'],
+      },
+      block1: {
+        id: 'block1',
+        blockId: 'edx_block-1',
+        lmsWebUrl: 'http://localhost/weburl',
+        legacyWebUrl: 'http://localhost/legacy',
+        studentViewUrl: 'http://localhost/studentview',
+        displayName: 'Block children 1',
+        type: 'sequential',
+      },
+      block2: {
+        id: 'block2',
+        blockId: 'edx_block-2',
+        lmsWebUrl: 'http://localhost/weburl',
+        legacyWebUrl: 'http://localhost/legacy',
+        studentViewUrl: 'http://localhost/studentview',
+        type: 'sequential',
+        displayName: 'Block children 2',
+      },
+    };
+
+    test('correctly formats blocks with path information', () => {
+      const formattedBlocks = formatBlocks(mockBlocks, 'blockRoot');
+      expect(formattedBlocks.block1.path).toBeDefined();
+      expect(formattedBlocks.block2.path).toBeDefined();
+    });
+
+    test('correctly assigns parentId to blocks', () => {
+      const formattedBlocks = formatBlocks(mockBlocks, 'blockRoot');
+      expect(formattedBlocks.block1.parentId).toBeDefined();
+      expect(formattedBlocks.block2.parentId).toBeDefined();
+    });
+
+    test('returns an empty object when blocks are empty', () => {
+      const formattedBlocks = formatBlocks({}, 'blockRoot');
+      expect(formattedBlocks).toEqual({});
+    });
+
+    test('handles invalid input gracefully', () => {
+      const formattedBlocks = formatBlocks(mockBlocks, 'nonExistingRoot');
+      expect(formattedBlocks.blockRoot.parentId).toBeNull();
+      expect(formattedBlocks.block1.parentId).toBeNull();
+      expect(formattedBlocks.block2.parentId).toBeNull();
+    });
+
+    test('maintains the original structure of blocks', () => {
+      const formattedBlocks = formatBlocks(mockBlocks, 'blockRoot');
+      expect(formattedBlocks.block1.id).toEqual('block1');
+      expect(formattedBlocks.block1.displayName).toEqual('Block children 1');
     });
   });
 
