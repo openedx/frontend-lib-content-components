8f467670b2c2033bc6eedddfc1284b85a4dbf063
Jesper Hodge <jhodge@outlook.de>
2024-03-11 16:09:53 +0000

    fix: lint

--- src/editors/sharedComponents/SelectableBox/utils.js
+++ src/editors/sharedComponents/SelectableBox/utils.js
@@ -7,21 +7,21 @@ import FormCheckboxSet from './FormCheckboxSet';
 export const getInputType = (component, type) => {
   if (component === 'SelectableBox') {
     switch (type) {
-    case 'radio':
-      return RadioControl;
-    case 'checkbox':
-      return CheckboxControl;
-    default:
-      return RadioControl;
+      case 'radio':
+        return RadioControl;
+      case 'checkbox':
+        return CheckboxControl;
+      default:
+        return RadioControl;
     }
   } else if (component === 'SelectableBoxSet') {
     switch (type) {
-    case 'radio':
-      return FormRadioSet;
-    case 'checkbox':
-      return FormCheckboxSet;
-    default:
-      return FormRadioSet;
+      case 'radio':
+        return FormRadioSet;
+      case 'checkbox':
+        return FormCheckboxSet;
+      default:
+        return FormRadioSet;
     }
   }
 };
